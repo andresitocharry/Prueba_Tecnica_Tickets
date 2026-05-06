014768d3d1523a85640e9157d7957956aa8a25ea
andres <a.charryc@uniandes.edu.co>
2026-05-06 13:42:41 -0500

    Corregir LOGIN_URL y configurar variables de entorno para producción

--- ticket_system/settings.py
+++ ticket_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^^f_@ffd8+31l#$sgzrskijgt3q3ja5qr-7puqwuhju&m61c#)'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^^f_@ffd8+31l#$sgzrskijgt3q3ja5qr-7puqwuhju&m61c#)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -129,3 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
