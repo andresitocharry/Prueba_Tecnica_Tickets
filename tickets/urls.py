2366b3073416d5d5af126d2be5f0634c23c57383
andres <a.charryc@uniandes.edu.co>
2026-05-06 13:17:30 -0500

    fix linter

--- tickets/urls.py
+++ tickets/urls.py
@@ -3,14 +3,14 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    
+
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('registro/', views.registro, name='registro'),
-    
+
     path('ticket/crear/', views.ticket_crear, name='ticket_crear'),
     path('mis-tickets/', views.ticket_listar, name='ticket_listar'),
     path('mis-tickets/<int:pk>/', views.ticket_detalle, name='ticket_detalle'),
-    
+
     path('', views.ticket_listar, name='home'),
 ]
